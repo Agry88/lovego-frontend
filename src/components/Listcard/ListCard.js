0b1abf11a296e896a5c35c69619a0e3178954015
agry88 <rgok307085@gmail.com>
2022-10-04 19:47:14 +0800

    Test:Style

--- src/components/Listcard/ListCard.js
+++ src/components/Listcard/ListCard.js
@@ -4,7 +4,8 @@ import shopcar from "../../imgs/main-menu.png";
 import tempjson from "../../temp.json";
 import { useEffect, useState } from "react";
 
-function ListCard() {
+function ListCard(props) {
+    const { color , title , img} = props;
 
     const [prods, setprods] = useState([])
 
@@ -15,10 +16,10 @@ function ListCard() {
     return (
         <section className="cateprod-list">
             <div className="cateprod-container">
-                <div className="cateprod-list-title-block">
-                    <span className="list-title">中秋特惠</span>
+                <div className="cateprod-list-title-block" style={{background : color ?? "#CD3F3F"}}>
+                    <span className="list-title">{title ?? "中秋特惠"}</span>
                     <button>查看更多商品 〉</button>
-                    <img src={festival_img} alt="cateprod-list-title-img" />
+                    <img src={img ?? festival_img} alt="cateprod-list-title-img" />
                 </div>
                 <div className="cateprod-list-product-block">
                     {prods && prods.slice(3, 7).map(prod => {
--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -5,6 +5,7 @@ import CardList from '../components/Card/cardList';
 import ListCard from '../components/Listcard/ListCard';
 import Footer from '../components/Footer/Footer';
 import Popular from '../components/Popular/Popular';
+import medicine_img from "../imgs/medicine.png";
 
 function Mainpage() {
   return (
@@ -19,8 +20,8 @@ function Mainpage() {
           <CardList />
         </div>
         <div className="round-section">
-          <ListCard />
-          <ListCard />
+          <ListCard color="#CD3F3F" title="中秋特惠"/>
+          <ListCard color="#619266" title="養生保健" img={medicine_img}/>
         </div>
         <div className="round-section">
           <Popular />
